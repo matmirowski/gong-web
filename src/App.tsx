7b12bb9b77a7427c281857d9093683e9c8b586b9
Szymon Tarnowski <szymontarno@gmail.com>
2024-05-05 12:27:54 +0200

    [2.c] Setup React Router and Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Landing</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          {/* Route for LandingPage (Main Page) */}
+          <Route path="/" element={<LandingPage />} />
+
+          {/* Route for HomePage */}
+          <Route path="/home" element={<HomePage />} />
+
+          {/* Redirect to LandingPage if path doesn't match */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
