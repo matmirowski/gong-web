c1fd2f8b86db8b11dd41d1ed21694b613cd6d504
Kacper Woźnica <kacper.woznica002@gmail.com>
2024-05-14 20:56:46 +0200

    [5] user can register

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 
 import './App.css';
 import Sign from './pages/SignInPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/sign" element={<Sign/>} />
+          <Route path="/register" element={<RegisterPage/>} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     </BrowserRouter>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Box from '../components/general/Box';
-import SignInForm from '../components/sign/SignInForm';
 import { Link } from 'react-router-dom';
 import Button, { ButtonState } from '../components/general/Button';
 import Navbar from '../components/general/Navbar';
+import RegisterForm from '../components/Register/RegisterForm';
 
-const Sign = () => {
+const RegisterPage = () => {
     return (
         <>
             <Navbar>
                     <Button onClick={() => console.log('Navigating...')} state={ButtonState.Active} width='136' height='35'>
-                        <Link to="/register">Zarejestruj</Link>
+                        <Link to="/sign">Zaloguj</Link>
                     </Button>
                 </Navbar>
             <Box>
-                <SignInForm/>
+                <RegisterForm/>
             </Box>
         </>
     );
 };
 
-export default Sign;
+export default RegisterPage;
