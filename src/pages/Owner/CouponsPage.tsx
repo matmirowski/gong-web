35223444b1426bfd50f28621141d55fb34da16b5
Szymon Tarnowski <szymontarno@gmail.com>
2024-06-02 19:56:30 +0200

    [42] Coupons designs adjustment

--- src/pages/Owner/CouponsPage.tsx
+++ src/pages/Owner/CouponsPage.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import CouponsList from '../../components/coupons/CouponsList';
-import useAuth from '../../hooks/useAuth';
-import Navbar from '../../components/general/Navbar';
-import Button, { ButtonState } from '../../components/general/Button';
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import CouponsList from "../../components/coupons/CouponsList";
+import useAuth from "../../hooks/useAuth";
+import Navbar from "../../components/general/Navbar";
+import Button, { ButtonState } from "../../components/general/Button";
 
 interface Coupon {
   id: number;
@@ -22,21 +22,24 @@ const CouponsPage: React.FC = () => {
     const fetchCoupons = async () => {
       if (userId !== null && token !== null) {
         try {
-          const response = await fetch(`http://localhost:3030/branches/${branchId}/coupons`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
+          const response = await fetch(
+            `http://localhost:3030/branches/${branchId}/coupons`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
             }
-          });
+          );
           const data = await response.json();
           if (Array.isArray(data)) {
             setCoupons(data);
           } else {
-            console.error('Unexpected data format:', data);
+            console.error("Unexpected data format:", data);
           }
         } catch (error) {
-          console.error('Error fetching coupons:', error);
+          console.error("Error fetching coupons:", error);
         } finally {
           setLoading(false);
         }
@@ -53,18 +56,28 @@ const CouponsPage: React.FC = () => {
   return (
     <>
       <Navbar>
-      <Link to={'/home'}>
-          <Button onClick={() => null} state={ButtonState.Active} width='136' height='35'>
-            Home
+        <Button
+          onClick={() => console.log("Navigating to home...")}
+          state={ButtonState.Active}
+          width="136"
+          height="35"
+        >
+          <Link to="/home">Główna</Link>
+        </Button>
+        <Link to="/">
+          <Button
+            onClick={() => {
+              console.log("Logging out...");
+              localStorage.removeItem("jwtToken");
+              window.location.reload();
+            }}
+            state={ButtonState.Active}
+            width="136"
+            height="35"
+          >
+            Wyloguj
           </Button>
         </Link>
-        <Button onClick={() => {
-          console.log('Logging out...');
-          localStorage.removeItem('jwtToken');
-          window.location.reload();
-        }} state={ButtonState.Active} width='136' height='35'>
-          <Link to="/">Wyloguj</Link>
-        </Button>
       </Navbar>
       <CouponsList coupons={coupons} />
     </>
